2285aa3e3a9af166a5b4b2159c0ec867dcb50b59
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-08-05 09:59:03 +0530

    Update index.js

--- JAVASCRIPT/Dicee Challenge - Starting Files/index.js
+++ JAVASCRIPT/Dicee Challenge - Starting Files/index.js
@@ -8,3 +8,17 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "./images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 2 wins"
+}
+else{
+    document.querySelector("h1").innerHTML = "Match Draw"
+}
