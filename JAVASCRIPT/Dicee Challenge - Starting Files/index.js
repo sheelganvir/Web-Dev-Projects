c060f6abb5cc218bb042b70b21ced42bf9fc8294
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-08-05 09:45:43 +0530

    Made changes in dicee

--- JAVASCRIPT/Dicee Challenge - Starting Files/index.js
+++ JAVASCRIPT/Dicee Challenge - Starting Files/index.js
@@ -1,3 +1,10 @@
-var randomNumber1 = Math.random();
-console.log("randomNumber");
-document.querySelector("img").setAttribute("img1", )
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage ="dice" + randomNumber1 +".png";  //dice1.png - dice2.png
+
+var randomImageSource = "./images/" + randomDiceImage;    //.images/dice1png - .images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
