b379e773c7353ba0e91c938f2da64ca3531ecaae
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-08-19 12:48:02 +0530

    initial commit

---
+++ Backend Development/Node.js/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`I am ${name}!`);
