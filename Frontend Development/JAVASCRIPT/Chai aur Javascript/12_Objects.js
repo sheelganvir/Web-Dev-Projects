3a3285e2313f08c5db3f533bbe15e71c19cc3eef
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-06 23:14:18 +0530

    update js

--- Frontend Development/JAVASCRIPT/Chai aur Javascript/12_Objects.js
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/12_Objects.js
@@ -7,7 +7,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Sheel",
     "full name": "Sheel Ganvir",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",  //Way to use symbol as a key of object
     age: 20,
     location: "Balaghat",
     email: "sheelg@gmail.com",
@@ -15,6 +15,28 @@ const JsUser = {
     lastLoginDays: ["Monday", "Friday"]
 }
 
+console.log(JsUser.email);  // Generally we use this 
+console.log(JsUser["email"]);   //best way to print 
+console.log(JsUser["full name"]);   
+console.log(JsUser[mySym]);
+console.log(typeof JsUser[mySym]);
+
+JsUser.email = "hitesh@gmail.com"   //change key value
+Object.freeze(JsUser)   //freeze the email
 console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
+JsUser.email = "hiteshsheel@gmail.com"
+console.log(JsUser);
+
+
+//*****Functions***** */
+
+JsUser.greeting = function(){
+    console.log("Hello JS Users");
+}
+
+JsUser.greetingTwo = function (){
+    console.log(`Hello Js User, ${this.name}`);     // We use "this" only when we have to refer in the same object
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
---
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/13_Objects2.js
@@ -0,0 +1,22 @@
+//const tinderUser = new Object()
+/*
+const tinderUser = {}
+
+tinderUser.id = "12345"
+tinderUser.name = "Sheel"
+tinderUser.IsLoggedIn = false
+
+console.log(tinderUser);
+*/
+
+//****** Nested objects ****** */
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstName: "Sheel",
+            lastName: "Ganvir"
+        }
+    }
+}
