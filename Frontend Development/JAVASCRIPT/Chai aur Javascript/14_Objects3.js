5cc06c80c9c48a3b5a0ea2c90b7c58f1640d8a18
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-07 18:50:35 +0530

    Update 14_Objects3.js

---
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/14_Objects3.js
@@ -0,0 +1,16 @@
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    Instructor: "Hitesh"
+}
+
+// One way to console out 
+console.log(course.Instructor);
+
+//Another way to console out
+const {Instructor} = course
+console.log(Instructor);
+
+//Change the key name
+const {Instructor: Ins} = course
+console.log(Ins);
