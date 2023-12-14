762a6ef25342ce9a9b051bcc86708f66437db3a0
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-14 16:17:22 +0530

    Update 25_HighOrder_ArrayLoops.js

--- Frontend Development/JAVASCRIPT/Chai aur Javascript/25_HighOrder_ArrayLoops.js
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/25_HighOrder_ArrayLoops.js
@@ -1,6 +1,6 @@
 /********************* for of loop ****************** */
 
-const arr = [1,2,3,4,5]
+const arr = [10,20,30,40,50]
 for (let i of arr) {
     console.log(i); 
 }
@@ -12,3 +12,51 @@ for(const greet of greetings){
 
 /************* Maps ***************** */
 
+const map = new Map()
+map.set("Name", "John Doe")
+map.set("Age","30")
+//map.set("Name", "John Doe")   map print only unique set of values
+console.log(map);
+
+for (const key of map) {
+    console.log(key);
+}
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+// Note: for of loop doesn't work for object
+
+/****************** for in loop *******************/
+
+//for in loop in object
+const myObj = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb : 'ruby'
+}
+for(const key in myObj) {
+    console.log(key);
+}
+for(const key in myObj) {
+    console.log(myObj[key]);
+}
+for(const key in myObj) {
+    console.log(key + " :- " + myObj[key]);
+}
+
+//for in loop in array
+const cricketer = ["Virat","dhoni","maxi"] 
+for (const key in cricketer) {
+    console.log(key);   //it prints keys not values but for of loop prints values directly
+}
+for (const key in cricketer) {
+    console.log(cricketer[key]);   
+}
+
+//Note: for in loop doesn't work for maps
+
+/*********************** for each loop ********************* */
+
+const coding = ["js","ruby","java","python"]
