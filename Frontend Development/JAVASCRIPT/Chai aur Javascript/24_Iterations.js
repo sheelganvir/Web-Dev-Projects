ab5ed46cf85f4811c1d2a5dbdd03a8367b3d606c
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-14 09:35:09 +0530

    update Iterations in js

--- Frontend Development/JAVASCRIPT/Chai aur Javascript/24_Iterations.js
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/24_Iterations.js
@@ -14,3 +14,23 @@ for (let index = 1; index <= 20; index++) {
     }  
     console.log(`Value of i is ${index}`);  
 }
+
+/************* while loop ************** */
+let index = 0;
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index = index+2
+}
+
+/************** do while loop *********** */
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
+
+let scoreNew = 11
+do {
+    console.log(`Score = ${scoreNew}`);
+    scoreNew++
+} while (scoreNew<=10);
