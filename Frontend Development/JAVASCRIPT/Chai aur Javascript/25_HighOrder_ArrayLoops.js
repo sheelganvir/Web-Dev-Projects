64b909a67448a29b200f0d3e3581e7e7daa45b5e
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-14 09:49:04 +0530

    File shifting --> basics to main

---
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/25_HighOrder_ArrayLoops.js
@@ -0,0 +1,14 @@
+/********************* for of loop ****************** */
+
+const arr = [1,2,3,4,5]
+for (let i of arr) {
+    console.log(i); 
+}
+
+const greetings = "Hello World"
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`);
+}
+
+/************* Maps ***************** */
+
