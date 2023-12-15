66c46f932c4e88994dd5d6e46d6a72f520dfd07e
Sheel Ganvir <sheelganvir2805@gmail.com>
2023-12-15 09:49:23 +0530

    Update 25_HighOrder_ArrayLoops.js

--- Frontend Development/JAVASCRIPT/Chai aur Javascript/25_HighOrder_ArrayLoops.js
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/25_HighOrder_ArrayLoops.js
@@ -60,3 +60,40 @@ for (const key in cricketer) {
 /*********************** for each loop ********************* */
 
 const coding = ["js","ruby","java","python"]
+
+coding.forEach(function (item) {
+    console.log(item);
+})
+coding.forEach( (item) => {
+    console.log(item);
+})
+
+function printMe(item){
+    console.log(item);
+}
+coding.forEach(printMe)
+
+coding.forEach((item,index,arr) => {
+    console.log(item,index,arr);
+})
+
+/*****************Objects inside an array*************  */
+
+const myCoding= [
+    {
+        languageName: "javascript",
+        filename: "js"
+    },
+    {
+        languageName: "java",
+        filename: "java"
+    },
+    {
+        languageName: "python",
+        filename: "py"
+    }
+]
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
+
