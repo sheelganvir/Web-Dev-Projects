0f39ecbe6e3b8fff3de6c14bf11054a30ceaa072
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-05-23 18:44:23 +0530

    update lec3

---
+++ Backend Development/Chai aur Backend/Lec3/backend/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req, res) => {
+    res.send('Server is ready');
+});
+
+// get a list of 5 jokes
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'This is another joke'
+        }
+    ]
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`)
+})
