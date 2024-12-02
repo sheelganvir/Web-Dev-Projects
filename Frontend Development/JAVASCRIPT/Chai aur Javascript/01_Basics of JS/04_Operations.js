53b98d63a759cc0fec33d1e4dff329f9be6bbb6c
Sheel Ganvir <128175450+sheelganvir@users.noreply.github.com>
2024-12-02 16:16:20 +0530

    Update 04_Operations.js

--- Frontend Development/JAVASCRIPT/Chai aur Javascript/01_Basics of JS/04_Operations.js
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/01_Basics of JS/04_Operations.js
@@ -14,12 +14,15 @@ let str3 = str1+ str2
 console.log(str3)   //Hello Sheel
 
 console.log("1"+"2");   //12
-console.log(1+"2");     //12
-console.log("1"+2+2);   //122
-console.log(1+2+"2");   //32
-console.log(3+4*5%3);   5
+console.log(1+"2");     //12 -> JavaScript converts the number to a string and performs string concatenation.
+console.log("1"+2+2);   //122 -> JavaScript converts the second operand (2) to a string and performs string concatenation, resulting in "12".
+                                //Then, it concatenates "12" with the third operand (2), producing "122".
+console.log(1+2+"2");   //32 -> The first two operands are numbers (1 and 2).
+                                  // JavaScript performs numeric addition, resulting in 3.
+                                  // Then, it encounters a string ("2") and converts 3 to a string to perform string concatenation.
+console.log(3+4*5%3);   //5 -> operator precedence: * > % > +
 
 // ************** TRICKY OPERATIONS ******************
 
-console.log(+true);     //1
-console.log(+"");       //0
+console.log(+true);     //1 -> the unary + operator attempts to convert its operand into a number.
+console.log(+"");       //0 -> The unary + operator converts the empty string to the number 0.
