273defca0b724e795553e03fa400282309cc236a
Sheel Ganvir <128175450+sheelganvir@users.noreply.github.com>
2024-12-02 16:54:06 +0530

    Update 05_Comparisons.js

--- Frontend Development/JAVASCRIPT/Chai aur Javascript/01_Basics of JS/05_Comparisons.js
+++ Frontend Development/JAVASCRIPT/Chai aur Javascript/01_Basics of JS/05_Comparisons.js
@@ -3,11 +3,11 @@
 /*  
     BASICS:
 
-    console.log(2>1) => 1
+    console.log(2>1) => true
     console.log(3>=4) => false
     console.log('hello'==='world') => false
     console.log(!true) => false
-    console.log(5<=6 && 7>=8) => true
+    console.log(5<=6 && 7>=8) => false
     console.log("2">1) => true
     console.log("02">1) => true
 */
@@ -24,6 +24,6 @@
     console.log(undefined<0) => false
     console.log(null===undefined) => false
     console.log(null!==undefined) => true
-    console.log(null!=undefined) => true
+    console.log(null!=undefined) => false, In loose equality/inequality, null and undefined are considered equal.
     console.log(null == undefined) => true    
 */
